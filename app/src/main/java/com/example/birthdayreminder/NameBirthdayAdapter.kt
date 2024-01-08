b821ba03fd055644c6c1bfb432a6cdc44513ce7e
Ramesh Abeysekara <47961710+rameshabeysekara@users.noreply.github.com>
2024-01-07 20:19:44 -0500

    add files

---
+++ app/src/main/java/com/example/birthdayreminder/NameBirthdayAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.birthdayreminder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NameBirthdayAdapter(private val nameBirthdayList: List< MainActivity.NameBirthday >,private val onItemClick: (String) -> Unit ) :
+    RecyclerView.Adapter<NameBirthdayAdapter.MyViewHolder>() {
+
+    class MyViewHolder( itemView: View ) : RecyclerView.ViewHolder( itemView ) {
+
+        val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+        val birthdayTextView: TextView = itemView.findViewById(R.id.birthdayTextView)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_name_birthday, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val nameBirthday = nameBirthdayList[position]
+        holder.nameTextView.text = nameBirthday.name
+        holder.birthdayTextView.text = nameBirthday.birthday
+        holder.itemView.setOnClickListener { onItemClick(nameBirthday.id) }
+    }
+
+    override fun getItemCount(): Int {
+        return nameBirthdayList.size
+    }
+}
---
+++ app/src/main/java/com/example/birthdayreminder/NameDateDialogFragment.kt
@@ -0,0 +1,75 @@
+package com.example.birthdayreminder
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class NameDateDialogFragment : DialogFragment() {
+
+    private var selectedDate : String = ""
+    private var onNameDateSetListener : ( ( name : String, birthday : String ) -> Unit )? = null
+
+    @SuppressLint("CutPasteId")
+    override fun onCreateView(
+        inflater : LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate( R.layout.fragment_name_date_dialog, container, false )
+        val nameEditText     = view.findViewById< EditText >( R.id.nameEditText )
+        val selectDateButton = view.findViewById< Button >( R.id.selectDateButton )
+        val saveButton       = view.findViewById< Button >( R.id.saveButton )
+
+        selectDateButton.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        saveButton.setOnClickListener {
+            val enteredName = nameEditText.text.toString()
+            if ( enteredName.isNotBlank() && selectedDate.isNotBlank() ) {
+                onNameDateSetListener?.invoke( enteredName, selectedDate )
+                dismiss()
+            }
+        }
+
+        return view
+
+    }
+
+    fun setOnNameDateSetListener( listener : ( name : String, birthday : String ) -> Unit ) {
+        onNameDateSetListener = listener
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val year     = calendar.get( Calendar.YEAR )
+        val month    = calendar.get( Calendar.MONTH )
+        val day      = calendar.get( Calendar.DAY_OF_MONTH )
+
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val sdf = SimpleDateFormat( "MMM. dd, yyyy", Locale.getDefault() )
+                val selectedDateCalendar = Calendar.getInstance()
+                selectedDateCalendar.set( selectedYear, selectedMonth, selectedDay )
+                selectedDate = sdf.format( selectedDateCalendar.time )
+
+                val changeTextButton = view?.findViewById< Button >( R.id.selectDateButton )
+                if ( changeTextButton != null ) {
+                    changeTextButton.text = selectedDate
+                }
+            },
+            year, month, day
+        )
+        datePickerDialog.show()
+    }
+}
+
